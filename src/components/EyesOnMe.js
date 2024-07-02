f16973c03a3ed6c1199a1d58257a9e9fa32742ea
Farha-rashid-10 <farkaf07@gmail.com>
2024-07-02 14:13:51 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+function EyesOnMe(){
+    function focus(){
+        console.log('Good!');
+    }
+    function handleEvent(){
+        console.log('Hey! Eyes on me!')
+    }
+    return <button onBlur={handleEvent} onFocus={focus}>Eyes on me</button>
+
+}
+export default EyesOnMe;
